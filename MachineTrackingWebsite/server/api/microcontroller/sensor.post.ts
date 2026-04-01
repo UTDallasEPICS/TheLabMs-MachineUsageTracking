c688d0860002c1337f288387dabb06359eb47058
LongHaa <122176872+LongHaa@users.noreply.github.com>
2026-04-01 18:27:35 -0500

    Add debugger to test input

--- MachineTrackingWebsite/server/api/microcontroller/sensor.post.ts
+++ MachineTrackingWebsite/server/api/microcontroller/sensor.post.ts
@@ -4,6 +4,9 @@ export default defineEventHandler(async (event) => {
   const mc = event.context.microcontroller
   const { machine_state } = await readBody(event)
 
+  // DEBUG: log incoming signal
+  console.log(`[DEBUG] Signal received from microcontroller "${mc.name ?? mc.id}": ${machine_state ? 'ON' : 'OFF'}`)
+
   const data = await prisma.sensorData.create({
     data: {
       microcontroller_id: mc.id,
